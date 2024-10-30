1e694f198a970e0b3ab1ed47d08b44e25b707c7a
dandekarsneha07 <dandekarsneha07@gmail.com>
2024-10-30 14:30:47 +0530

    Update sys_array_4x4.v

--- sys_array_4x4.v
+++ sys_array_4x4.v
@@ -1,4 +1,4 @@
-`include "PE_booth.v"
+`include "PE.v"
 module systolic_array(A0, A1, A2, A3, B0, B1, B2, B3, C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, clk, reset);
 
 //inout output ports
